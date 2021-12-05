057d8e5ccc439b10fa5268e74212ac657198a5dc
Jeffrey Kozik <jeffreykozik@pm.me>
2021-12-05 14:22:58 -0500

    Update inLine.py
    updated getinline function to work for multiple businesses and services

--- inLine.py
+++ inLine.py
@@ -37,7 +37,7 @@ def createService(service_name, business_phone_num):
 # createService("Dinner", "2167952355")
 
 def getInLine(service_name, business_phone_num, customer_phone_num):
-    mycursor.execute("SELECT count(*) FROM inline")
+    mycursor.execute("SELECT count(*) FROM inline WHERE business_phone_num = " + business_phone_num + "AND service_name = " + service_name)
     people_in_line = mycursor.fetchone()[0]
     print(people_in_line)
     sql = "INSERT INTO inline (service_name, business_phone_num, customer_phone_num, position, minutes_left) VALUES (%s, %s, %s, %s, %s)"
